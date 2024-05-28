fc734290ac174968c75d2c641086f05f604aa050
Daniel Brackenbury <daniel.brackenbury@gmail.com>
2024-05-28 00:11:20 -0400

    moved sway scripts

--- sway/lock-and-blur.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# start by screenshotting the screen
-grim - > ~/Scripts/swaylock/screenshot.jpg
-# now blur it
-convert ~/Scripts/swaylock/screenshot.jpg -blur 0x16 ~/Scripts/swaylock/screenshot.jpg
-# now lock the screen
-swaylock -f -i ~/Scripts/swaylock/screenshot.jpg
