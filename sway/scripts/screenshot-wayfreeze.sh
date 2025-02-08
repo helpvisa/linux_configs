b4c7b5e5eef6c6e2cfd5a2587e2f083479144f41
helpvisa <daniel.brackenbury@gmail.com>
2025-02-08 02:15:51 -0500

    update wayfreeze+slurp script

--- sway/scripts/screenshot-wayfreeze.sh
+++ sway/scripts/screenshot-wayfreeze.sh
@@ -2,4 +2,4 @@
 
 # maim -s | xclip -selection clipboard -t image/png
 # grim -g "$(slurp)" - | wl-copy
-./wayfreeze & PID=$!; sleep .1; grim -g "$(slurp)" - | wl-copy; kill $PID
+wayfreeze --hide-cursor & PID=$!; sleep .1; grim -g "$(slurp)" - | wl-copy; kill $PID
