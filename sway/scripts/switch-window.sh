7aeedfa421989bd0a2a4ef802665f265dd66e985
Daniel Brackenbury <daniel.brackenbury@gmail.com>
2025-02-20 01:07:06 -0500

    update switch window script to toggle inactive windows w same title

--- sway/scripts/switch-window.sh
+++ sway/scripts/switch-window.sh
@@ -5,6 +5,6 @@ echo "$WINDOW"
 APP_ID="$(printf "%s" "$WINDOW" | awk 'BEGIN {FS=": ";}{print $1}')"
 NAME="$(printf "%s" "$WINDOW" | awk 'BEGIN {FS=": ";}{print $2}')"
 
-wlrctl toplevel focus "app_id:${APP_ID}" "title:${NAME}"
+wlrctl toplevel focus "app_id:${APP_ID}" "title:${NAME}" "state:inactive"
 # we echo to trim whitespace
 echo "app_id:${APP_ID}" "title:${NAME}"
