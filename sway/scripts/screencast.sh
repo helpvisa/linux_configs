5804cf28568e3f185110969f7d06ee26ad87ec08
Daniel Brackenbury <daniel.brackenbury@gmail.com>
2025-02-20 20:29:31 -0500

    update screencast and screenshot scripts; screenshot now also saves images

--- sway/scripts/screencast.sh
+++ sway/scripts/screencast.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 IS_RECORDING="$(pgrep -l wf-recorder)"
-FILENAME="$HOME/Videos/Screencasts/$(date | sed 's/ /_/g;s/:/-/g').mp4"
+FILENAME="$HOME/Videos/Screencasts/Screencast From $(date | sed 's/ /_/g;s/:/-/g').mp4"
 
 if [ -z "$IS_RECORDING" ]; then
     notify-send "wf-recorder" "Recording started! Saving to $FILENAME" -t 500
--- sway/scripts/screenshot-wayfreeze.sh
+++ sway/scripts/screenshot-wayfreeze.sh
@@ -3,3 +3,6 @@
 # maim -s | xclip -selection clipboard -t image/png
 # grim -g "$(slurp)" - | wl-copy
 wayfreeze --hide-cursor & PID=$!; sleep .1; grim -g "$(slurp)" - | wl-copy; kill $PID
+# also save to disk
+FILENAME="$HOME/Pictures/Screenshots/Screenshot From $(date | sed 's/ /_/g;s/:/-/g').png"
+wl-paste > "$FILENAME"
