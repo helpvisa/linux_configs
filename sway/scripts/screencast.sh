b4629128c25e4ae0dd118f171792c7c708e04353
Daniel Brackenbury <daniel.brackenbury@gmail.com>
2025-02-20 20:08:43 -0500

    add screencast.sh for wayland WMs, which uses wf-recorder

---
+++ sway/scripts/screencast.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+IS_RECORDING="$(pgrep -l wf-recorder)"
+FILENAME="$HOME/Videos/Screencasts/$(date | sed 's/ /_/g;s/:/-/g').mp4"
+
+if [ -z "$IS_RECORDING" ]; then
+    notify-send "wf-recorder" "Recording started! Saving to $FILENAME" -t 500
+    wf-recorder -g "$(slurp)" -f "$FILENAME"
+else
+    killall wf-recorder
+    notify-send "wf-recorder" "Recording finished!"
+fi
