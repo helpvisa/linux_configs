2c4efa8a2cca656559f2c1966059d60a6b42a994
Daniel Brackenbury <daniel.brackenbury@gmail.com>
2025-02-21 20:03:37 -0500

    allow titles with colons to appear in switch-window.sh script

--- sway/scripts/switch-window.sh
+++ sway/scripts/switch-window.sh
@@ -3,7 +3,7 @@
 WINDOW="$(wlrctl toplevel list | fuzzel -d)"
 echo "$WINDOW"
 APP_ID="$(printf "%s" "$WINDOW" | awk 'BEGIN {FS=": ";}{print $1}')"
-NAME="$(printf "%s" "$WINDOW" | awk 'BEGIN {FS=": ";}{print $2}')"
+NAME="$(printf "%s" "$WINDOW" | awk '{split($0,f,": "); sub(/^([^: ]+: )/,"",$0); print $0}')"
 
 wlrctl toplevel focus "app_id:${APP_ID}" "title:${NAME}" "state:inactive"
 # we echo to trim whitespace
