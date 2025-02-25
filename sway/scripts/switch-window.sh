e87f60433327b509e8ff83abd119eb4cd98d8f87
Daniel Brackenbury <daniel.brackenbury@gmail.com>
2025-02-25 10:29:22 -0500

    update placeholder text in switch-window.sh

--- sway/scripts/switch-window.sh
+++ sway/scripts/switch-window.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-WINDOW="$(wlrctl toplevel list | fuzzel -d)"
+WINDOW="$(wlrctl toplevel list | fuzzel -d --placeholder='search through windows...')"
 echo "$WINDOW"
 APP_ID="$(printf "%s" "$WINDOW" | awk 'BEGIN {FS=": ";}{print $1}')"
 NAME="$(printf "%s" "$WINDOW" | awk '{split($0,f,": "); sub(/^([^: ]+: )/,"",$0); print $0}')"
