afe4ddc7023bc7f35a294da73a9efa91393ed29d
Daniel Brackenbury <daniel.brackenbury@gmail.com>
2025-02-27 01:41:36 -0500

    change gnome switch windows to use standard Window Calls extension

--- gnome/switch-windows-gnome.sh
+++ gnome/switch-windows-gnome.sh
@@ -1,21 +1,20 @@
 #!/bin/sh
 # requires jq,
 # activate-window-by-title extension,
-# and window-calls-extended extension
+# and window-calls extension
 
 # head and tail strip brackets + quotes at beginning and end that ruin the json
 # WindowsExt.List has a nasty tendency to escape quotes randomly
 # sed can fix this behaviour
 LIST_RAW=$(gdbus call --session --dest org.gnome.Shell \
-        --object-path /org/gnome/Shell/Extensions/WindowsExt \
-        --method org.gnome.Shell.Extensions.WindowsExt.List \
+        --object-path /org/gnome/Shell/Extensions/Windows \
+        --method org.gnome.Shell.Extensions.Windows.List \
         | head -c -4 | tail -c +3 | sed 's/\\"/"/g')
-LIST=$(printf "%s" "$LIST_RAW" | jq -r '.[] | "\(.class): \(.title)"')
+LIST=$(printf "%s" "$LIST_RAW" | jq -r '.[] | "\(.wm_class): \(.title)"')
 
 # use picker to select window; must be GNOME compatible so no fuzzel :(
 WINDOW=$(printf "%s" "$LIST" | fzf)
 SELECTION=$(printf "%s" "$WINDOW" | awk '{split($0,f,": "); sub(/^([^: ]+: )/,"",$0); print $0}')
-printf "%s\n" "$SELECTION"
 gdbus call --session \
     --dest org.gnome.Shell \
     --object-path /de/lucaswerkmeister/ActivateWindowByTitle \
