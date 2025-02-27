09bf50be06161bb4c94e401bf7cbba7728d5a602
Daniel Brackenbury <daniel.brackenbury@gmail.com>
2025-02-27 00:59:41 -0500

    add app IDs to selector

--- gnome/switch-windows-gnome.sh
+++ gnome/switch-windows-gnome.sh
@@ -10,12 +10,14 @@ LIST_RAW=$(gdbus call --session --dest org.gnome.Shell \
         --object-path /org/gnome/Shell/Extensions/WindowsExt \
         --method org.gnome.Shell.Extensions.WindowsExt.List \
         | head -c -4 | tail -c +3 | sed 's/\\"/"/g')
-LIST=$(printf "%s" "$LIST_RAW" | jq -r '.[].title')
+LIST=$(printf "%s" "$LIST_RAW" | jq -r '.[] | "\(.class): \(.title)"')
 
 # use picker to select window; must be GNOME compatible so no fuzzel :(
 WINDOW=$(printf "%s" "$LIST" | fzf)
+SELECTION=$(printf "%s" "$WINDOW" | awk '{split($0,f,": "); sub(/^([^: ]+: )/,"",$0); print $0}')
+printf "%s\n" "$SELECTION"
 gdbus call --session \
     --dest org.gnome.Shell \
     --object-path /de/lucaswerkmeister/ActivateWindowByTitle \
     --method de.lucaswerkmeister.ActivateWindowByTitle.activateBySubstring \
-    "$(printf "%s" "$WINDOW")"
+    "$(printf "%s" "$SELECTION")"
