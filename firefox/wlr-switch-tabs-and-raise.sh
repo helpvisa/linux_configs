c39fc794d1cc5693ff4fd8cf9d75db87f9029a40
helpvisa <daniel.brackenbury@gmail.com>
2025-02-28 13:12:54 -0500

    fix wlr-switch-tabs-and-raise behaviour

--- firefox/wlr-switch-tabs-and-raise.sh
+++ firefox/wlr-switch-tabs-and-raise.sh
@@ -14,12 +14,20 @@ else
     ~/.local/bin/brotab activate "$TAB_ID"
     if [ $? -ne 0 ]; then
         firefox --new-tab "https://duckduckgo.com/?q=${SELECTION}"
+        # this value may need changing depending on machine
+        sleep 0.2
+        TAB_NAME="$SELECTION"
+    else
+        TAB_NAME=$(printf "%s" "$SELECTION" | awk '{split($0,array,"\t"); print array[2]}')
     fi
 
     # now raise the window (wlr-specific)
-    TAB_NAME=$(printf "%s" "$SELECTION" | awk '{split($0,array,"\t"); print array[2]}')
-    APP_DETAILS=$(wlrctl toplevel list | grep "$(printf "%s" "$TAB_NAME")")
+    printf "%s\n" "$TAB_NAME"
+    APP_DETAILS=$(wlrctl toplevel list | grep "$TAB_NAME")
+    printf "%s\n" "$APP_DETAILS"
     APP_ID="$(printf "%s" "$APP_DETAILS" | awk 'BEGIN {FS=": ";}{print $1}')"
+    # printf "%s\n" "$APP_ID"
     NAME="$(printf "%s" "$APP_DETAILS" | awk '{split($0,f,": "); sub(/^([^: ]+: )/,"",$0); print $0}')"
+    # printf "%s\n" "$NAME"
     wlrctl toplevel focus "app_id:${APP_ID}" "title:${NAME}" "state:inactive"
 fi
