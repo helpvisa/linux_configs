b67246a1542bec22a1019641ed55c32d3282f645
Daniel Brackenbury <daniel.brackenbury@gmail.com>
2025-02-28 01:19:09 -0500

    not selecting anything in the firefox script allows creating new firefox search

--- firefox/switch-tabs-and-raise.sh
+++ firefox/switch-tabs-and-raise.sh
@@ -10,7 +10,11 @@ if [ -z "$SELECTION" ]; then
     printf "%s\n" "no selection made!"
 else
     TAB_ID=$(printf "%s" "$SELECTION" | awk '{print $1}')
+
     brotab activate "$TAB_ID"
+    if [ $? -ne 0 ]; then
+        firefox --new-tab "https://duckduckgo.com/?q=${SELECTION}"
+    fi
 
     # now raise the window (gnome-specific
     WINDOW_NAME=$(printf "%s" "$SELECTION" | awk '{split($0,array,"\t"); print array[2]}')
