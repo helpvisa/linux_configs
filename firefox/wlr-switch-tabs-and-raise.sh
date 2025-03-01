3f422b39c239cf4a78896c3e7222ea947fe30bf8
helpvisa <daniel.brackenbury@gmail.com>
2025-03-01 03:10:02 -0500

    expand scope of wlr-switch-tabs-and-raise.sh to auto-start firefox and go to websites if no matching tabs found

--- firefox/wlr-switch-tabs-and-raise.sh
+++ firefox/wlr-switch-tabs-and-raise.sh
@@ -3,10 +3,11 @@
 # requires activate-window-by-title
 
 LIST=$(~/.local/bin/brotab list)
+# SELECTION=$(printf "%s" "$LIST" | fzf --style=minimal --layout=reverse --margin 5% --prompt='activate tab: ')
 # SELECTION=$(printf "%s" "$LIST" | fuzzel -d --placeholder='activate browser tab')
 SELECTION=$(printf "%s" "$LIST" \
     | BEMENU_BACKEND=wayland bemenu -i -l 30 -f \
-    -H 24 \
+    -H 25 \
     --counter=always \
     -p 'activate tab <>' \
     --fn 'Input Mono 12' \
@@ -29,22 +30,30 @@ else
     TAB_ID=$(printf "%s" "$SELECTION" | awk '{print $1}')
 
     ~/.local/bin/brotab activate "$TAB_ID"
-    if [ $? -ne 0 ]; then
-        firefox --new-tab "https://duckduckgo.com/?q=${SELECTION}"
+    if [ $? -ne 0 ] || [ -z "$(~/.local/bin/brotab clients)" ]; then
+        if printf "%s" "$SELECTION" | grep -q "\."; then
+            URL="${SELECTION}"
+        else
+            URL="https://duckduckgo.com/?q=${SELECTION}"
+        fi
+
+        if ! pgrep firefox; then
+            nohup firefox "${URL}" >/dev/null 2>&1 &
+            echo "opening new firefox instance"
+        else
+            nohup firefox --new-tab "${URL}" >/dev/null 2>&1 &
+        fi
+
         # this value may need changing depending on machine
-        sleep 0.2
+        sleep 0.3
         TAB_NAME="$SELECTION"
     else
         TAB_NAME=$(printf "%s" "$SELECTION" | awk '{split($0,array,"\t"); print array[2]}')
     fi
 
     # now raise the window (wlr-specific)
-    printf "%s\n" "$TAB_NAME"
     APP_DETAILS=$(wlrctl toplevel list | grep "$TAB_NAME")
-    printf "%s\n" "$APP_DETAILS"
     APP_ID="$(printf "%s" "$APP_DETAILS" | awk 'BEGIN {FS=": ";}{print $1}')"
-    # printf "%s\n" "$APP_ID"
     NAME="$(printf "%s" "$APP_DETAILS" | awk '{split($0,f,": "); sub(/^([^: ]+: )/,"",$0); print $0}')"
-    # printf "%s\n" "$NAME"
     wlrctl toplevel focus "app_id:${APP_ID}" "title:${NAME}" "state:inactive"
 fi
