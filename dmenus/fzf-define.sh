10be8310c1fd23a341077abc7607218ad519f516
Daniel Brackenbury <daniel.brackenbury@gmail.com>
2025-03-24 22:06:24 -0400

    add keybinds to scroll preview window in fzf-define

--- dmenus/fzf-define.sh
+++ dmenus/fzf-define.sh
@@ -1,4 +1,6 @@
-echo "" | fzf --bind=enter:replace-query+print-query \
+echo "" | fzf \
+    --multi \
+    --bind "enter:replace-query+print-query,ctrl-n:preview-down,ctrl-p:preview-up,ctrl-d:preview-page-down,ctrl-u:preview-page-up" \
     --style=minimal \
     --layout=reverse --margin 3% --prompt='query definition: ' \
     --preview='dict {q}' --preview-window 'bottom,99%'
