66fdd0854602745a76a44ba0f16bd0f48428dd06
helpvisa <daniel.brackenbury@gmail.com>
2025-04-18 14:16:13 -0400

    improve toggle-touchpad.sh for gnome

--- gnome/toggle-touchpad.sh
+++ gnome/toggle-touchpad.sh
@@ -1,11 +1,24 @@
 #!/bin/sh
 
 STATUS=$(gsettings get org.gnome.desktop.peripherals.touchpad send-events)
+ID=$(cat /tmp/toggle-touchpad-id 2>/dev/null)
 
 if [ "$STATUS" = "'enabled'" ]; then
     gsettings set org.gnome.desktop.peripherals.touchpad send-events "disabled"
-    notify-send "gsettings" "Touchpad Disabled" -t 2500
+    COMMAND="notify-send -a 'Touchpad' 'OFF' 'Touchpad disabled!' -t 2500 -e"
+    if [ -n "$ID" ]; then
+        COMMAND="$COMMAND -r $ID -p > /tmp/toggle-touchpad-id"
+    else
+        COMMAND="$COMMAND -p > /tmp/toggle-touchpad-id"
+    fi
+    eval "$COMMAND"
 else
     gsettings set org.gnome.desktop.peripherals.touchpad send-events "enabled"
-    notify-send "gsettings" "Touchpad Enabled" -t 2500
+    COMMAND="notify-send -a 'Touchpad' 'ON' 'Touchpad enabled!' -t 2500 -e"
+    if [ -n "$ID" ]; then
+        COMMAND="$COMMAND -r $ID -p > /tmp/toggle-touchpad-id"
+    else
+        COMMAND="$COMMAND -p > /tmp/toggle-touchpad-id"
+    fi
+    eval "$COMMAND"
 fi
