c136f59473308a4f3f1a93b3148f5ceb9523617c
Daniel Brackenbury <daniel.brackenbury@gmail.com>
2025-05-13 17:28:37 -0400

    fix up natural scroll + leftie mode scripts for gnome

--- gnome/toggle-left-handed.sh
+++ gnome/toggle-left-handed.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 
 CURRENT_SETTING="$(gsettings get org.gnome.desktop.peripherals.mouse left-handed)"
+ID=$(cat /tmp/toggle-trackball-id 2>/dev/null)
 
 if [ "$CURRENT_SETTING" == "true" ]; then
     gsettings set org.gnome.desktop.peripherals.mouse left-handed false
-    notify-send "gsettings" "Disabled leftie mode!"
+    COMMAND="notify-send -a 'gsettings' 'OFF' 'Disabled leftie mode!' -t 2500 -e"
+    if [ -n "$ID" ]; then
+        COMMAND="$COMMAND -r $ID -p > /tmp/toggle-trackball-id"
+    else
+        COMMAND="$COMMAND -p > /tmp/toggle-trackball-id"
+    fi
+    eval "$COMMAND"
 else
     gsettings set org.gnome.desktop.peripherals.mouse left-handed true
-    notify-send "gsettings" "Enabled leftie mode!"
+    COMMAND="notify-send -a 'gsettings' 'ON' 'Enabled leftie mode!' -t 2500 -e"
+    if [ -n "$ID" ]; then
+        COMMAND="$COMMAND -r $ID -p > /tmp/toggle-trackball-id"
+    else
+        COMMAND="$COMMAND -p > /tmp/toggle-trackball-id"
+    fi
+    eval "$COMMAND"
 fi
--- gnome/toggle-natural-scroll.sh
+++ gnome/toggle-natural-scroll.sh
@@ -1,15 +1,28 @@
 #!/bin/bash
 
 CURRENT_SETTING="$(gsettings get org.gnome.desktop.peripherals.mouse natural-scroll)"
+ID=$(cat /tmp/toggle-trackball-id 2>/dev/null)
 
 if [ "$CURRENT_SETTING" == "true" ]; then
     # disable natural scroll
     gsettings set org.gnome.desktop.peripherals.mouse natural-scroll false
-    gsettings set org.gnome.desktop.peripherals.mouse speed "-0.516447368421"
-    notify-send "gsettings" "Disabled trackball mode!"
+    # gsettings set org.gnome.desktop.peripherals.mouse speed "-0.516447368421"
+    COMMAND="notify-send -a 'gsettings' 'OFF' 'Natural scroll disabled!' -t 2500 -e"
+    if [ -n "$ID" ]; then
+        COMMAND="$COMMAND -r $ID -p > /tmp/toggle-trackball-id"
+    else
+        COMMAND="$COMMAND -p > /tmp/toggle-trackball-id"
+    fi
+    eval "$COMMAND"
 else
     # enable natural scroll
     gsettings set org.gnome.desktop.peripherals.mouse natural-scroll true
-    gsettings set org.gnome.desktop.peripherals.mouse speed "-0.25"
-    notify-send "gsettings" "Enabled trackball mode!"
+    # gsettings set org.gnome.desktop.peripherals.mouse speed "-0.25"
+    COMMAND="notify-send -a 'gsettings' 'ON' 'Natural scroll enabled!' -t 2500 -e"
+    if [ -n "$ID" ]; then
+        COMMAND="$COMMAND -r $ID -p > /tmp/toggle-trackball-id"
+    else
+        COMMAND="$COMMAND -p > /tmp/toggle-trackball-id"
+    fi
+    eval "$COMMAND"
 fi
