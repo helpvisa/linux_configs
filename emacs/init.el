c5648463fdeefa55a735b4559aea0e2915d7a54d
Daniel Brackenbury <daniel.brackenbury@gmail.com>
2025-05-16 17:01:59 -0400

    emacs init.el: add alternative comment-line keybind

--- emacs/init.el
+++ emacs/init.el
@@ -458,6 +458,7 @@ corresponding to the characters of this string are shown."
 (evil-define-key 'normal 'global (kbd "<SPC> A") 'connect-or-disconnect-lsp)
 (evil-define-key 'normal 'global (kbd "<SPC> a") 'flycheck-mode)
 ;; setup extra evil keybinds 
+(define-key my/keys-keymap (kbd "C-c C-g") 'comment-line)
 (evil-define-key 'normal 'global "gcc" 'comment-line)
 (evil-define-key 'visual 'global "gc" 'comment-or-uncomment-region)
 (define-key my/keys-keymap (kbd "C-c C-r") 'recentf)
