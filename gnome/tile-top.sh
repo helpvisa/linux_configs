b784f63df164a1e1af19b646b4f3916a56d70cae
Daniel Brackenbury <daniel.brackenbury@gmail.com>
2026-04-01 23:15:23 -0400

    update gnome/tile-* scripts to fix Discord threads breaking them

--- gnome/tile-top.sh
+++ gnome/tile-top.sh
@@ -4,10 +4,12 @@
 # head and tail strip brackets + quotes at beginning and end that ruin the json
 # WindowsExt.List has a nasty tendency to escape quotes randomly
 # sed can fix this behaviour
+# in fact, sometimes you have to double-up on the sed... Discord threads
+# bizarrely require this, doubling up the escaped double quotes
 LIST_RAW=$(gdbus call --session --dest org.gnome.Shell \
         --object-path /org/gnome/Shell/Extensions/Windows \
         --method org.gnome.Shell.Extensions.Windows.List \
-        | head -c -4 | tail -c +3 | sed 's/\\"/"/g')
+        | head -c -4 | tail -c +3 | sed 's/\\"/"/g' | sed 's/\\"/"/g')
 FOCUS=$(printf "%s" "$LIST_RAW" \
     | jq -r '.[] | select( .focus == true ) | "\(.id)"')
 
